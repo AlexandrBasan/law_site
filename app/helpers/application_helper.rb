4adaff025f109b15477addaaac1141e943a810a0
Proalab <alexandr.basan@gmail.com>
2014-05-22 15:04:03 +0300

    law-assistant

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "BestLaw group"
+    base_title = "Law Assistant"
     if page_title.empty?
       base_title
     else
